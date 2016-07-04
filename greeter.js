00d5e3fe2a710d5fb44909144fc05ed2dc3c3ae7
D.Park Fiaworld <D.Park Fiaworld>
2016-07-04 15:35:40 +0900

    add id in greeter.html

--- greeter.js
+++ greeter.js
@@ -1,9 +1,9 @@
 function greeter(person) {
     return "Hello, " + person;
 }
-var 홍길동 = "Jane 홍길동";
+var Hong = "Jane Hong";
 var myNumber = 12;
-//document.getElementById('number').innerHTML=myNumber;
-//document.getElementById('myName').innerHTML = greeter(홍길동);
-document.body.innerHTML = greeter(홍길동);
+document.getElementById('number').innerHTML=myNumber;
+document.getElementById('myName').innerHTML = greeter(Hong);
+//document.body.innerHTML = greeter(Hong);
 //document.body.innerHTML = myNumber;
