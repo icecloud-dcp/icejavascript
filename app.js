c615e84cf842455141762ff12e9422894e2ccbe4
D.Park Fiaworld <D.Park Fiaworld>
2016-07-04 15:09:05 +0900

    modify all js files

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 var express = require('express');
 var app = express();
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User";
-document.body.innerHTML = greeter(user);
 
-app.get('/', function(req, res){
-    res.send('Hello express!');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-app.listen(3000, function() {
-    console.log('Listening on port 3000...');
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-var msg = 'hello world';
-console.log(msg);
-   // var http = require('http');
-   // http.createServer(function(request, response) {
-   // response.writeHead(200);
-    //response.write("Hello, this is dog.");
-   // response.end();
-   // }).listen(3000);
-//console.log('Listening on port 3000...');
--- greeter.js
+++ greeter.js
@@ -1,5 +1,9 @@
 function greeter(person) {
     return "Hello, " + person;
 }
-var user = "Jane User";
-document.body.innerHTML = greeter(user);
+var 홍길동 = "Jane 홍길동";
+var myNumber = 12;
+//document.getElementById('number').innerHTML=myNumber;
+//document.getElementById('myName').innerHTML = greeter(홍길동);
+document.body.innerHTML = greeter(홍길동);
+//document.body.innerHTML = myNumber;
